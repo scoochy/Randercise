db5106db1fb291606978c8e615e227f41ca4a7d7
Scoochy <52045665+scoochy@users.noreply.github.com>
2021-05-27 13:22:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+"""RANDERCISE"""
+
+#Get user input for difficulty selection
+
+#From difficulty selected, select random exercises from each DB table
+
+#Easy:
+#   sets
+#   roundTime
+#   restTime
+#   restTimeSets
+#   2 exercises from elements n to m of table upperBody
+#   2 exercises from elements n to m of table core_andAbs
+#   2 exercises from elements n to m of table lowerBody
+
+#Medium:
+#   sets
+#   roundTime
+#   restTime
+#   restTimeSets
+#   3 exercises from elements n to m of table upperBody
+#   3 exercises from elements n to m of table core_andAbs
+#   3 exercises from elements n to m of table lowerBody
+
+#Hard:
+#   4 exercises from elements n to m of table upperBody
+#   4 exercises from elements n to m of table core_andAbs
+#   4 exercises from elements n to m of table lowerBody
+
+#Preview workout
+#   Sequence exercises with 1 from each table after another
+#   for total amount of exercises selected
+#
+#   Display exercise sequence along with roundNumber
+#    Display roundTime and restTime
+#
+#   Ex:
+#      Easy: upperBody,core_andAbs,lowerBody,upperBody,core_andAbs,lowerBody
+#      Rounds:3 of each
+#      Round Time: 30s and Rest Time:45s
+#      Rest Time between rounds
+
+#Select Start to begin circuit
+#For each exercise, display:
+#       Name:
+#       Roundtime:
+#       Pause Button
+#       Stop  Button
