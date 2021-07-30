6ce0152794cf0e2546473c76965e3a2698f37be3
Scoochy <52045665+scoochy@users.noreply.github.com>
2021-07-30 15:33:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import threading
-
+import os
+os.environ['KIVY_VIDEO'] = 'ffpyplayer'
 import kivy
-import time
+kivy.require('1.9.0')
 import random
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 import sqlite3
-from kivy.properties import ObjectProperty
+
+from kivy.app import App
 from kivy.clock import Clock
+from kivy.lang import Builder
 from kivy.uix.accordion import Accordion, AccordionItem
-from kivy.properties import ListProperty
-
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 con = sqlite3.connect('exercises.db')
 
 cur = con.cursor()
+
 
 #cur.execute('''CREATE TABLE exercises (name text, difficulty text, muscles text)''')
 
@@ -62,9 +57,6 @@ class MainWindow(Screen):
     exercise_names = []
     random_exercises = []
 
-
-
-
     def level(self, difficulty, number):
         self.difficulty = difficulty
         self.num_to_select = number
@@ -87,80 +79,87 @@ class Exercises(Accordion):
 
 class SecondWindow(Screen):
 
-
     def on_enter(self, *args):
         if self.my_callback() == False:
             exerlist = Exercises.exercise_list(self)
             self.ids.grid.add_widget(exerlist)
 
     def my_callback(dt):
-        print(MainWindow.exercise_names), dt
         if len(MainWindow.exercise_names) > 0:
-            print(MainWindow.exercise_names[0])
             return False
 
     event = Clock.schedule_interval(my_callback, 1)
 
 
 class ThirdWindow(Screen):
-    play = 0
+    setnumber = 1
+    onpause = 3
+    runningclock = 0
 
+    def on_enter(self, *args):
+        self.ids.timer.text = str(self.onpause)
+        self.function_interval = Clock.schedule_interval(self.first_countdown, 1)
+        self.ids.exercise.text = MainWindow.exercise_names[(self.setnumber - 1)]
+        self.ids.next.text = MainWindow.exercise_names[(self.setnumber)]
 
-    def on_enter(self):
-        self.function_interval = Clock.schedule_interval(self.countdown, 1)
-        Clock.schedule_once(self.stop_timer, 3)
-        self.ids.exercise.text = MainWindow.exercise_names[0]
-        self.ids.next.text = MainWindow.exercise_names[1]
+    def first_countdown(self, *args):
+        self.onpause -= 1
+        self.ids.timer.text = str(self.onpause)
+        if self.ids.timer.text == "0":
+            Clock.unschedule(self.function_interval)
+            Clock.unschedule(self.first_countdown)
+            self.runningclock += 1
+            self.onpause = 3
+            self.event = Clock.schedule_interval(self.countdown, 1)
 
-
-
-    def stop_timer(self, *args):
-        self.function_interval.cancel()
-        self.set_number()
-        self.reset_timer()
-
-
-    def countdown(self, *args):
-        self.ids.timer.text = str(int(self.ids.timer.text) - 1)
-
-    def set_number(self):
-        Clock.schedule_interval(self.change_set, 30)
 
 
     def change_set(self, *args):
-        self.ids.set.text = str(int(self.ids.set.text) + 1)
-        self.ids.exercise.text = MainWindow.exercise_names[int(self.ids.set.text) - 1]
-        if self.ids.set.text == str(len(MainWindow.exercise_names)):
+        if self.setnumber == len(MainWindow.exercise_names):
             self.ids.next.text = "You're almost there!"
+            self.ids.set.text = str(self.setnumber)
             return False
         else:
-            self.ids.next.text = MainWindow.exercise_names[int(self.ids.set.text)]
+            self.ids.next.text = MainWindow.exercise_names[self.setnumber]
+            self.ids.set.text = str(self.setnumber)
+            self.ids.exercise.text = MainWindow.exercise_names[(self.setnumber - 1)]
 
+    def countdown(self, *args):
+        self.ids.timer.text = str(self.onpause)
+        self.onpause -= 1
+        if self.ids.timer.text == "0":
+            self.onpause = 3
+            self.setnumber += 1
+            if self.setnumber > len(MainWindow.exercise_names):
+                self.change_screen()
+            else:
+                self.change_set()
 
-    def reset_timer(self, *args):
-        self.ids.timer.text = "30"
-        Clock.schedule_interval(self.timer_loop, 30)
-        Clock.schedule_interval(self.set_countdown, 1)
-
-
-    def set_countdown(self, *args):
-        if self.pause == False:
-            self.ids.timer.text = str(int(self.ids.timer.text) - 1)
+    def pause_play(self):
+        if self.runningclock == 1:
+            self.ids.pause.text = "Play"
+            Clock.unschedule(self.event)
+            self.runningclock -= 1
         else:
-            Clock.unschedule(self.set_countdown)
+            self.runningclock += 1
+            self.ids.pause.text = "Pause ||"
+            Clock.schedule_once(self.event)
 
-    def timer_loop(self, *args):
-        self.ids.timer.text = "30"
-
-    def pause(self, *args):
-        if self.play > 0:
-            Clock.schedule_interval(self.set_countdown, 1)
-            return False
-
-
+    def change_screen(self):
+        if self.manager.current == 'timer':
+            self.manager.current = 'gif'
+        else:
+            self.manager.current = 'timer'
 
 class FourthWindow(Screen):
-    pass
+    def on_enter(self, *args):
+        Clock.schedule_once(self.change_screen, 5)
+
+    def change_screen(self):
+        if self.manager.current == 'gif':
+            self.manager.current = 'cooldown'
+        else:
+            self.manager.current = 'gif'
 
 class FifthWindow(Screen):
     pass
@@ -172,11 +171,12 @@ kv = Builder.load_file("my.kv")
 
 class MyMainApp(App):
     sm = WindowManager()
+
+
     def build(self):
         return kv
         return sm
-    def on_pause(self):
-        return True
+
 
 
 
